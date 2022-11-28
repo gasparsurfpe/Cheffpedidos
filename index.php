a10ca0879d04cfb33f18f5e20bbcb8c6759826f2
Luiz Augusto <gasparsurfpe@gmail.com>
2022-11-28 12:12:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang=“pt-br”>
+<head>
+    <meta charset=“utf-8”/>
+    <meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
+    <title>CHEFF PEDIDOS -  Sistema para Controle de Pedidos</title>
+    <link href=“” rel=“stylesheet”/>
+    <script src=“scripts/seu-script.js”></script>
+</head>
+<body>
+
+
+</body>
+</html>
