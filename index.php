1774a0456d52ed7407f4fe712442ee5220f20a83
Luiz Augusto <gasparsurfpe@gmail.com>
2022-12-07 14:32:38 -0300

    Atualização

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
     <meta charset=“utf-8”/>
     <meta content=“width=device-width, initial-scale=1, maximum-scale=1” name=“viewport”>
     <title>CHEFF PEDIDOS -  Sistema para Controle de Pedidos</title>
-    <link href=“” rel=“stylesheet”/>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <script src=“scripts/seu-script.js”></script>
 </head>
 <body>
+<div class="contend">
+    <div class="container">
+        <form class="form-control" action="">
+            <label for="usu">Usuário:</label>
+            <input class="form-control" type="text" name="usu" id="usu">
+            <br>
+                    <!--usuario e senha campos-->
+            <label for="sen">Senha:</label>
+            <input class="form-control" type="password" name="sen"><br>
+            <div class="d-grid">
+            <button type="submit" class="btn btn-primary btn-block">Acessar</button>
+            </div>
+        </form>
+    </div>
+
+
+</div>
 
 
 </body>
